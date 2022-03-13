239136c1f8140c704cce200a50aee6b46bcc5ab4
Albertojserr <alberto.j.serrano@gmail.com>
2022-03-13 18:12:09 +0100

    cAMBIOS RAROS

---
+++ prueba.py
@@ -0,0 +1,36 @@
+
+"""
+1. Run `env/bin/pip install pyramid_jinja2`
+2. Copy this template and put it in `templates/home.jinja2`:
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>{{ saludos }}</title>
+</head>
+<body>
+<h1>{{ greet }}, {{ saludos }}</h1>
+</body>
+</html>
+"""
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.view import view_config
+
+
+@view_config(
+    route_name='home',
+    renderer='templates/home.jinja2'
+)
+def home(request):
+    return {"greet": 'Bienvenido', "name": 'Pepe',"saludos":'Hola',"prueba":'Prueba'}
+
+if __name__ == '__main__':
+    with Configurator() as config:
+        config.include('pyramid_jinja2')
+        config.add_route('home', '/')
+        config.scan()
+        app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', 6543, app)
+    server.serve_forever()
+
